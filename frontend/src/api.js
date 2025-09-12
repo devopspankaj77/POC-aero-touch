7a3632316d5ba63d735ea0df5b370118c37eaf9a
pankaj <pankaj.tiwari.devops@gmail.com>
2025-09-12 11:55:27 +0530

    port correction

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// backend service URL (Kubernetes me service name use hoga: http://backend:3000)
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:3000";
+// backend service URL
+const API_URL = process.env.REACT_APP_API_URL || "http://192.168.49.2:30081";
 
 export async function getMessage() {
   const response = await axios.get(`${API_URL}/api/message`);
